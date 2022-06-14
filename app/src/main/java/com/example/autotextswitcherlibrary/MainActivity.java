8667ec461fa2d516381f41e4ba3cbadf76a981af
MasoudSaraf <masoudsarraf513@gmail.com>
2022-06-14 20:43:21 +0430

    completed example module

--- app/src/main/java/com/example/autotextswitcherlibrary/MainActivity.java
+++ app/src/main/java/com/example/autotextswitcherlibrary/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.autotextswitcherlibrary;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatTextView;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Gravity;
+
+import com.masoud.autotextswitcher.AutoTextSwitcher;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -12,5 +17,17 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AutoTextSwitcher textSwitcher = findViewById(R.id.textSwitcher);
+        textSwitcher.setFactory(() ->
+        {
+            AppCompatTextView textView = new AppCompatTextView(MainActivity.this);
+            textView.setTextAppearance(MainActivity.this, R.style.InfoTextAppearanceStyle);
+            textView.setMinLines(2);
+            textView.setPadding(10, 10, 10, 10);
+            textView.setShadowLayer(12, 0, -5.5f, Color.parseColor("#65FFFFFF"));
+            textView.setGravity(Gravity.BOTTOM | Gravity.CENTER_HORIZONTAL);
+            return textView;
+        });
     }
 }
